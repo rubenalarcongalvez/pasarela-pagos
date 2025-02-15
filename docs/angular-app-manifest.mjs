9032b67ee7d1721f87a03e99a38e9065d5be2ebf
rubenalarcongalvez <rubenalarcongalvez@gmail.com>
2025-02-15 16:36:19 +0100

    subido index.html renombrado

--- docs/angular-app-manifest.mjs
+++ docs/angular-app-manifest.mjs
@@ -25,11 +25,11 @@ export default {
 ],
   entryPointToBrowserMapping: undefined,
   assets: {
-    'index.csr.html': {size: 2382, hash: '6f9cd6b94a93d69f53b9ffbb13a7a79b9f7836328a23be2c206df340c74ad6ad', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 1467, hash: '08bffdc94f60ca3eb554ec56e09d976137e29f5a89f7113750951e6b62890369', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
-    'ko-fi/index.html': {size: 25870, hash: '3de2a65e65c546c5f9796f17fc1ec590ab1500650800be68ce947cab6d2534cd', text: () => import('./assets-chunks/ko-fi_index_html.mjs').then(m => m.default)},
-    'stripe/index.html': {size: 24182, hash: 'a6fee9f8291e58b2ae89329492c2b1d36b585af58ebeeb67d2a1c760a55bb62a', text: () => import('./assets-chunks/stripe_index_html.mjs').then(m => m.default)},
-    'paypal/index.html': {size: 24182, hash: 'b1fd3d2e64d96503f76523870b096cb79cbfdbdd8939264aa2ab7da9a6e8d686', text: () => import('./assets-chunks/paypal_index_html.mjs').then(m => m.default)},
-    'styles-HDXMDPJA.css': {size: 17367, hash: 'bMo5xU4zFQM', text: () => import('./assets-chunks/styles-HDXMDPJA_css.mjs').then(m => m.default)}
+    'index.csr.html': {size: 2382, hash: 'fcae62b51eab3ec0eb6979cc708b431610845ebd0d5780c88bb9fde3a5f83ce4', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 1467, hash: '4555f9ef2cd27ffcabff51434197e3d22fc4116ec1920e7c244aab4c5272a3ca', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'paypal/index.html': {size: 24182, hash: 'fd31dbff1cf9e912013dbfe8d661e8354696fd05b1ce62e064eb5a98004bc42f', text: () => import('./assets-chunks/paypal_index_html.mjs').then(m => m.default)},
+    'stripe/index.html': {size: 24182, hash: '1a783ea0b14f9af0ad824689d1001486b1b2dae7f9b60c32ccec712e174f7a2f', text: () => import('./assets-chunks/stripe_index_html.mjs').then(m => m.default)},
+    'ko-fi/index.html': {size: 25972, hash: '5187760c7aad6fd5ae830c3a232bab4373a04f16b8f603f603fc114f8fc21826', text: () => import('./assets-chunks/ko-fi_index_html.mjs').then(m => m.default)},
+    'styles-Z6BCTSAP.css': {size: 17451, hash: 'aUCjiun63N8', text: () => import('./assets-chunks/styles-Z6BCTSAP_css.mjs').then(m => m.default)}
   },
 };
